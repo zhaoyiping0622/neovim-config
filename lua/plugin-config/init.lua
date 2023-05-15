35e89fc5e1cedfae0bcb9b3b66876090b098fab3
zhaoyiping <zhaoyiping0622@gmail.com>
2023-05-15 16:35:51 +0800

    remove nvim open_on_setup

--- lua/plugin-config/init.lua
+++ lua/plugin-config/init.lua
@@ -1,6 +1,5 @@
 require 'plugin-config/gruvbox'
 require 'plugin-config/telescope'
-require 'plugin-config/nvim-tree'
 require 'plugin-config/nvim-cmp'
 require 'plugin-config/lsp'
 require 'plugin-config/which-key'
